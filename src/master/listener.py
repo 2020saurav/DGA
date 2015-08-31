4dc5d7b2d09357de68f71054407e9a27b70d15cd
Saurav Kumar <2020saurav@gmail.com>
2015-08-31 17:12:12 +0530

    Handler for master and test for network call

---
+++ src/master/listener.py
@@ -0,0 +1,28 @@
+'''
+Network call handler of master server
+'''
+
+from socket import *
+import sys
+import thread
+sys.path.append('../../config')
+
+from networkParams import *
+
+def handler(sc, address):
+    messageLength = int(sc.recv(MESSAGE_LENGTH_DIGITS))
+    message = sc.recv(messageLength)
+    words = message.split(MESSAGE_DELIMITER)
+    print words
+    sc.send(MESSAGE_RECEIPT_SUCCESS)
+    sc.close()
+
+if __name__ == '__main__':
+    s = socket(AF_INET, SOCK_STREAM)
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    s.bind((HOST_IP, HOST_PORT))
+    s.listen(MAX_BACKLOG_CONN)
+    while True:
+        sc, address = s.accept()
+        thread.start_new_thread(handler, (sc, address))
+    s.close()
---
+++ test/networkCall.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+sys.path.append('../config')
+
+from networkParams import *
+
+def messageLength(message):
+    return str('%04d'%len(message))
+
+def request(IP, port, message):
+    s = socket.socket()
+    s.connect((IP, port))
+    s.send(messageLength(message))
+    s.send(message)
+
+if __name__ == '__main__':
+    IP = '127.0.0.1'
+    port = 2020
+    message = "Hello$world"
+    request(IP, port, message)
