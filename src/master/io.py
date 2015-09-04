46bca19804edd46a62c17d7843eae3beb653c672
Abhilash Kumar <abhi.iitk16@gmail.com>
2015-09-04 16:23:05 +0530

    Input file parser for master added

---
+++ src/master/io.py
@@ -0,0 +1,17 @@
+'''Module to handle all i/o's for master.'''
+import sys
+sys.path.append('../graph')
+sys.path.append('../../config')
+from graph import Graph
+def readInput():
+    '''First line contains two arguments
+    n = number of vertices
+    m = number of edges in the graph
+    next m line conatins (a,b) reresenting an edge.'''
+    n , m  = map(int,raw_input().split(" "))
+    edges = []
+    for i in range(0,m):
+        a , b = map(int,raw_input().split(" "))
+        edges.append((a,b))
+    graph = Graph(n,m,edges)
+    print graph.toString()
