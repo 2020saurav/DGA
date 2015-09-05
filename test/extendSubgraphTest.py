d755680ebc6d431450a8b957ee2439d4f8f50aa1
Abhilash Kumar <abhi.iitk16@gmail.com>
2015-09-05 18:33:36 +0530

    Extend graph subroutine tested!

--- test/extendSubgraphTest.py
+++ test/extendSubgraphTest.py
@@ -9,13 +9,31 @@ from graph import Graph
 from task import Task
 from extendSubgraph import ExtendSubgraph
 
-def testExtendSubgraph():
+def testExtendSubgraph1():
     graph = Graph(5,5,[(0,1),(1,2),(2,3),(3,4),(4,0)])
     extender = ExtendSubgraph(graph, 101, 3)
-    task = Task([0,1,1,0,0],[3, 4],0,0)
+    task = Task([0,1,1,0,0],[0,3],0,0)
+    newTasks = extender.generateNewTasks(task)
+    for task in newTasks:
+        print task.vertices, task.edges
+
+def testExtendSubgraph2():
+    graph = Graph(3,2,[(0,1),(1,2)])
+    extender = ExtendSubgraph(graph, 101, 3)
+    task = Task([1,1,1], [], 0, 0)
+    newTasks = extender.generateNewTasks(task)
+    for task in newTasks:
+        print task.vertices, task.edges
+
+def testExtendSubgraph3():
+    graph = Graph(3,3,[(0,1),(1,2),(0,2)])
+    extender = ExtendSubgraph(graph, 101, 3)
+    task = Task([1,1,1], [1], 0, 0)
     newTasks = extender.generateNewTasks(task)
     for task in newTasks:
         print task.vertices, task.edges
 
 if __name__ == '__main__':
-    testExtendSubgraph()
+    testExtendSubgraph1()
+    testExtendSubgraph2()
+    testExtendSubgraph3()
